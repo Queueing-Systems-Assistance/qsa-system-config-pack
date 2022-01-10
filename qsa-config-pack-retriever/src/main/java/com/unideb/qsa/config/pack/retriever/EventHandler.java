44a1487180ff1c564c634a8ffa4db6bb40815edc
Szilágyi Zoltán <zoltan.szilagyi.cse@gmail.com>
2022-01-10 22:49:00 +0100

    QSA-168 Revert previous commit
    This reverts commit 572893c1b5e607e4a5714b22ae7a16b05c0b645f.

--- qsa-config-pack-retriever/src/main/java/com/unideb/qsa/config/pack/retriever/EventHandler.java
+++ qsa-config-pack-retriever/src/main/java/com/unideb/qsa/config/pack/retriever/EventHandler.java
@@ -4,26 +4,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import com.unideb.qsa.domain.context.ConfigPack;
-import com.unideb.qsa.domain.deserializer.JsonDeserializerHelper;
-
 /**
  * Handler for AWS Lambda calls.
  */
-public class EventHandler implements RequestHandler<String, List<ConfigPack>> {
+public class EventHandler implements RequestHandler<String, String> {
 
     @Override
-    public List<ConfigPack> handleRequest(String input, Context context) {
+    public String handleRequest(String input, Context context) {
         var path = getConfigPath();
         try {
-            String content = Files.lines(path).collect(Collectors.joining("\n"));
-            return List.of(JsonDeserializerHelper.deserializeToConfigPack(content));
+            return Files.lines(path).collect(Collectors.joining("\n"));
         } catch (IOException e) {
             throw new RuntimeException("Cannot read config pack", e);
         }
